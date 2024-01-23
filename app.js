16b715b386b0b78e56eb84ac0d966d1a61853ee3
joshuagatizz <anggapramono01@gmail.com>
2024-01-23 18:11:12 +0700

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const shortenRouter = require("./routes/shorten")
 const redirectRouter = require("./routes/redirect")
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/", redirectRouter)
 app.use("/api/shorten", shortenRouter)
