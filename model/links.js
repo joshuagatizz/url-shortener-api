39e2303ba7b220fdabac991582f0f8fbc23346d9
joshuagatizz <anggapramono01@gmail.com>
2024-01-29 18:46:20 +0700

    switched to singleton connection for db

--- model/links.js
+++ model/links.js
@@ -1,29 +1,12 @@
-const { MongoClient } = require("mongodb");
-const {
-  MONGO_URI,
-  MONGO_DB_NAME,
-  MONGO_COLLECTION_NAME,
-} = process.env
-
-const createDbConnection = async () => {
-  const client = new MongoClient(MONGO_URI);
-
-  try {
-    await client.connect();
-    return client.db(MONGO_DB_NAME).collection(MONGO_COLLECTION_NAME);
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    throw error;
-  }
-};
+const dbConnection = require("./db")
 
 const getRedirectByKey = async (key) => {
-  const db = await createDbConnection();
+  const db = await dbConnection.getInstance()
   return await db.findOne({ "key": key })
 }
 
 const createNewRedirect = async (redirectLink) => {
-  const db = await createDbConnection();
+  const db = await dbConnection.getInstance();
   return await db.insertOne(redirectLink)
 }
 
